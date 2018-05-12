3d7341dd28ff69ce56eaa51d285684ba9d4d384e
Jorge Moller <jmoller@integraccs.com>
2018-05-12 17:42:46 -0300

    add form wizard

---
+++ nova/js/initFormWizard.js
@@ -0,0 +1,37 @@
+$('document').ready(function() {
+
+
+  $('button[type="submit"]').click(function(e) {
+    e.preventDefault();
+
+    var activeNav = $('.fw__nav  .active');
+    activeNav.addClass('success');
+    activeNav.next().addClass('active');
+    activeNav.removeClass('active'); 
+
+    
+    var targetForm = $(this).attr('data-target');
+    $('form.active').animateCss('fadeOutLeft', function() {
+      $(targetForm).prev().removeClass('active');
+      $(targetForm).attr('class','form animated fadeInRight active');
+    });
+    
+
+  });
+
+  $('.prev').click(function() {
+    var activeNav = $('.fw__nav  .active');
+    activeNav.removeClass('active');
+    activeNav.prev().addClass('active');
+    activeNav.prev().removeClass('success');
+debugger;
+    var targetForm = $(this).attr('data-target');
+    $('form.active').animateCss('fadeOutRight', function() {
+      $(targetForm).next().removeClass('active');
+      $(targetForm).attr('class', 'form animated fadeInLeft active');
+    });
+    
+  })
+
+
+});
